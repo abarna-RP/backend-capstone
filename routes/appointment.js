83744523f7d9107c7b8225eb367a5abb8710821c
abarna-RP <prabaabu62@gmail.com>
2025-04-08 19:23:05 +0530

    hello

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import Appointment from '../models/Appointment.js';
 import { verifyToken } from '../middleware/auth.js';
-
+import { generateAgoraToken } from '../utils/videoCall.js';
+import { body, validationResult } from 'express-validator';
 const router = express.Router();
 
 // Create appointment
-router.post('/', verifyToken, async (req, res) => {
+router.post('/', verifyToken,[ body('client').notEmpty().withMessage('Client is required'),
+body('counselor').notEmpty().withMessage('Counselor is required'),
+body('date').notEmpty().withMessage('Date is required'),
+body('sessionType').notEmpty().withMessage('Session type is required'),
+], async (req, res) => {
   try {
     const appointment = new Appointment(req.body);
     await appointment.save();
@@ -44,5 +49,17 @@ router.put('/notes/:appointmentId', verifyToken, async (req, res) => {
         res.status(500).send(error.message);
     }
 });
+router.get('/video-call/:appointmentId/:userId', verifyToken, async (req, res) => {
+  try {
+    const appointmentId = req.params.appointmentId;
+    const userId = req.params.userId;
 
+    const channelName = `appointment-${appointmentId}`;
+    const token = generateAgoraToken(channelName, Number(userId));
+
+    res.send({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 export default router;

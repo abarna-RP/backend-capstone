c8241c6cb18f644dced7448e6beb68745b5e5f01
abarna-RP <prabaabu62@gmail.com>
2025-04-10 10:23:59 +0530

    changes

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import dotenv from "dotenv";
 import Payment from '../models/Payment.js';
-import { verifyToken } from '../middleware/auth.js';
+
 
 dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);

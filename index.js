35fcc86619282177fb9ce81b3133fe82c5b1e8b7
abarna-RP <prabaabu62@gmail.com>
2025-04-10 13:49:40 +0530

    change

--- index.js
+++ index.js
@@ -26,22 +26,31 @@ mongoose
   .catch((err) => console.error('MongoDB Connection Error:', err));
 
 
-  app.use('/auth', authRoutes);
+app.use('/auth', authRoutes);
 app.use('/counselors', counselorRoutes);
 app.use('/clients', clientRoutes);
 app.use('/appointments', appointmentRoutes);
 app.use('/payments', paymentRoutes);
 app.get('/video-token', (req, res) => {
-  const channelName = req.query.channelName;
-  const uid = Number(req.query.uid);
+  const { channelName, uid } = req.query;
+
+  if (!channelName || !uid) {
+    return res.status(400).send({ error: 'channelName and uid are required' });
+  }
+
   try {
+    console.log('Generating token for:', channelName, uid);
     const token = generateAgoraToken(channelName, uid);
     res.send({ token });
   } catch (error) {
-    console.error("error generating token", error);
-    res.status(500).send({error : "token generation failed"});
+    console.error('Error generating token:', error);
+    res.status(500).send({ error: 'Token generation failed' });
   }
-
 });
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
+app.use((req, res) => {
+  console.log("404")
+  res.json({ message: '404' })
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
